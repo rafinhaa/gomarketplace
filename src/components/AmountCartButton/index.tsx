76c099c1f9f96518c386b1ac6d19e49d3cf8ffb5
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-01-01 13:01:47 -0300

    chore: Adiciona a estrutura do botão

---
+++ src/components/AmountCartButton/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {
+    Button,
+} from './styles';
+
+const AmountCartButton: React.FC = () => {
+    return (
+        <Button>
+            
+        </Button>
+    );
+}
+
+export default AmountCartButton;
---
+++ src/components/AmountCartButton/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+import { TouchableOpacity } from "react-native";
+
+export const Button = styled(TouchableOpacity)`
+
+`;
+
