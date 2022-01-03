7f4f9c6a8ed5e7ecc4ca9afac80f9d55256c0125
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-01-03 14:22:07 -0300

    chore: Adiciona componente AddItemCart ao storybook

--- storybook/stories/index.ts
+++ storybook/stories/index.ts
@@ -2,3 +2,4 @@ import './Button/Button.stories';
 import './Welcome/Welcome.stories';
 import './AmountCartButton/AmountCartButton.stories';
 import './CartItem/CartItem.stories';
+import './AddItemCart/AddItemCart.stories';
