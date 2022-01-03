16cb0868ca66f930a5b7bd47fbf98fd63a17f508
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-01-03 15:40:22 -0300

    chore: adiciona componente DashItem

---
+++ src/components/DashItem/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    background: ${({theme}) => theme.colors.shape};
+    padding: 16px;
+    border-radius: 5px;
+    height: 241px;
+    width: 156px;
+`;
+
+export const Title = styled.Text`
+    margin-top: 8px;
+    font-size: 14px;
+    color: ${({theme}) => theme.colors.title};
+    font-family: ${({theme}) => theme.fonts.primary};
+`;
+
+export const Price = styled.Text`
+    font-size: 16px;
+    color: ${({theme}) => theme.colors.primary};
+    font-family: ${({theme}) => theme.fonts.bold};
+`;
+
+export const Image = styled.Image.attrs({
+    resizeMode: 'contain',
+})`
+    width: 122px;
+    height: 122px;
+`;
+
+export const Header = styled.View`
+`;
+
+export const Footer = styled.View`
+    margin-top: 50px;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const Content = styled.View`
+`;
