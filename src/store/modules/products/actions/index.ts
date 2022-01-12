a8d3d0ae178cc999f818c0c9d03c6409db0601f3
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-01-12 18:00:36 -0300

    chore: Cria ação que remove ou diminiu o produto no carrinho

--- src/store/modules/products/actions/index.ts
+++ src/store/modules/products/actions/index.ts
@@ -1,8 +1,17 @@
-import { Product, ADD_PRODUCT} from '../types';
+import { Product, ADD_PRODUCT, REMOVE_PRODUCT} from '../types';
 
 export function addProductCart(product: Product) {
     return {
         type: ADD_PRODUCT,
+        payload: {
+            product
+        }
+    }
+}
+
+export function removeProductCart(product: Product) {
+    return {
+        type: REMOVE_PRODUCT,
         payload: {
             product
         }
