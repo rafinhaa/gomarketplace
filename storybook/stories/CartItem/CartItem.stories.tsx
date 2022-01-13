9c3a03c2b1b09601e196d670141b983f84eb44f8
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-01-13 14:58:55 -0300

    chore: Modifica o storybook para se adequar ao redux

--- storybook/stories/CartItem/CartItem.stories.tsx
+++ storybook/stories/CartItem/CartItem.stories.tsx
@@ -11,9 +11,10 @@ storiesOf('CartItem', module)
       product={{
         id: '1',
         title: 'Produto 1',
-        price: 'R$ 100.00',
+        price: '1',
         image: 'https://i.picsum.photos/id/1055/200/200.jpg?hmac=23b3LoSYozZgCujiEcPGpgSvTaW35YghR4_EK2eJU9w',
-        quantity: '1x R$ 100.00',
+        quantify: '1',
+        countCart: 1,
       }}
       onClickAdd={action('clicked-add')}
       onClickRemove={action('clicked-remove')}
